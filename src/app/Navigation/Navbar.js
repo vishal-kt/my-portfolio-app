a734e1652391888cd24d2ede1966708fb60cfd72
Vishal <31177415+vishal-kt@users.noreply.github.com>
2023-12-22 14:54:21 +0000

    update

--- src/app/Navigation/Navbar.js
+++ src/app/Navigation/Navbar.js
@@ -4,13 +4,8 @@ const Navbar = () => {
   return (
     <div className='h-9 bg-red-400 flex items-center justify-between ' >
         <div className='text-2xl font-semibold pl-2' >VISHAL KUMAR</div>
-<<<<<<< HEAD
-        <div className='flex gap-3 pr-3 text-blue-900 '> 
-            <h4>Projects</h4>
-=======
         <div className='flex gap-3 pr-3'> 
             <h4>About</h4>
->>>>>>> origin/master
             <h4>Career</h4>
             <h4>Blog</h4>
             <h4>Contact</h4>
